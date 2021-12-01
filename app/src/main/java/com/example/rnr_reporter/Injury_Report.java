6973aaab8f6e3b03a6cae27d38f216fd90b3e929
Robert Eley <r.eley21@gmail.com>
2021-12-01 16:55:33 -0600

    Begin Implementing Time/Date Picker

--- app/src/main/java/com/example/rnr_reporter/Injury_Report.java
+++ app/src/main/java/com/example/rnr_reporter/Injury_Report.java
@@ -1,8 +1,10 @@
 package com.example.rnr_reporter;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 public class Injury_Report extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Injury_Report extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.injury_report);
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePicker();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePicker();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
--- app/src/main/java/com/example/rnr_reporter/Property_Damage.java
+++ app/src/main/java/com/example/rnr_reporter/Property_Damage.java
@@ -1,8 +1,10 @@
 package com.example.rnr_reporter;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 public class Property_Damage extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Property_Damage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.property_damage);
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePicker();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePicker();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
